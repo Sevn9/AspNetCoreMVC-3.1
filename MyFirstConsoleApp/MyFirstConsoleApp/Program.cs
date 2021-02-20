8fcad4db8517f682040fbbd04b55347a0cef592c
Zevn9 <dz3nls7@gmail.com>
2021-02-20 22:58:33 +0300

    created 1 and 2 prjct

---
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+namespace MyFirstConsoleApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      CreateHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    
+      Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+      {
+        webBuilder.UseStartup<Startup>();
+      });
+    
+
+  }
+}
